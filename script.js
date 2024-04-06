5b0d6d018e06516fad69219ef47a3c73a8a09c46
simonjuma_230 <simon.ombom22@students.dkut.ac.ke>
2024-04-06 23:48:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+//ADDING ELEMENTS INTO AN ARRAY LIST
+const friends = ['michael', 'juma', 'simon','rose'];
+friends.push('jay'); // Add an element into an array list in the last position
+
+console.log(friends);
+
+friends.shift('Jakes'); // Add an element into an array list in the begginging of an array
+console.log(friends);
+
+//REMOVING ELEMENTS IN AN ARRAY LIST
+friends.pop(); // Remove the last element in an array list
+friends.pop();
+console.log(friends);
+
